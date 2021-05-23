a3c444bb19229e3e5cf6fdda76fd54eaf80b2f45
Robin <robin.ingelbrecht@entityone.be>
2021-05-23 15:52:07 +0200

    Added lambda functions where possible

--- src/PsnProfileScraper.py
+++ src/PsnProfileScraper.py
@@ -30,7 +30,8 @@ class PsnProfileScraper:
         return Profile(
             self.__psn_name,
             country,
-            ProfileSummary.create_from_soup(BeautifulSoupFactory.create_from_string(str(self.__soup_profile) + str(self.__soup_stats))),
+            ProfileSummary.create_from_soup(
+                BeautifulSoupFactory.create_from_string(str(self.__soup_profile) + str(self.__soup_stats))),
             self.__get_recent_trophies(),
             self.__get_rarest_trophies(),
             self.__get_milestones(),
@@ -44,11 +45,8 @@ class PsnProfileScraper:
         if not self.__soup_profile.select("ul#recent-trophies"):
             return []
 
-        recent_trophies = []
-        for recent_trophy in self.__soup_profile.select("ul#recent-trophies")[0].find_all('li'):
-            recent_trophies.append(Trophy.create_from_alternative_soup(BeautifulSoupFactory.create_from_string(str(recent_trophy))))
-
-        return recent_trophies
+        return list(map(lambda trophy: Trophy.create_from_alternative_soup(BeautifulSoupFactory.create_from_string(str(trophy))),
+                        self.__soup_profile.select("ul#recent-trophies")[0].find_all('li')))
 
     # Scrapes and returns trophy cabinet
     def __get_rarest_trophies(self) -> List[Trophy]:
@@ -58,13 +56,8 @@ class PsnProfileScraper:
         if not self.__soup_profile.select('div.sidebar div.box.no-top-border table'):
             return []
 
-        rarest_trophies = self.__soup_profile.select('div.sidebar div.box.no-top-border table')[0]
-
-        trophies = []
-        for trophy in rarest_trophies.find_all("tr"):
-            trophies.append(Trophy.create_from_soup(BeautifulSoupFactory.create_from_string(str(trophy))))
-
-        return trophies
+        return list(map(lambda trophy: Trophy.create_from_soup(BeautifulSoupFactory.create_from_string(str(trophy))),
+                        self.__soup_profile.select('div.sidebar div.box.no-top-border table')[0].find_all("tr")))
 
     # Scrapes and returns trophy cabinet
     def __get_trophy_cabinet(self) -> List[Trophy]:
@@ -74,13 +67,8 @@ class PsnProfileScraper:
         if not self.__soup_profile.select("div.sidebar table.box.zebra"):
             return []
 
-        trophy_cabinet = self.__soup_profile.select("div.sidebar table.box.zebra")[0]
-
-        trophies = []
-        for trophy in trophy_cabinet.find_all("tr"):
-            trophies.append(Trophy.create_from_soup(BeautifulSoupFactory.create_from_string(str(trophy))))
-
-        return trophies
+        return list(map(lambda trophy: Trophy.create_from_soup(BeautifulSoupFactory.create_from_string(str(trophy))),
+                        self.__soup_profile.select("div.sidebar table.box.zebra")[0].find_all("tr")))
 
     # Scrapes and returns milestones.
     def __get_milestones(self) -> List[Milestone]:
@@ -89,11 +77,9 @@ class PsnProfileScraper:
 
         milestones_table = self.__soup_profile.find("h3", text="Trophy Milestones").find_next()
 
-        milestones = []
-        for milestone in milestones_table.find_all("tr"):
-            milestones.append(Milestone.create_from_soup(BeautifulSoupFactory.create_from_string(str(milestone))))
-
-        return milestones
+        return list(
+            map(lambda milestone: Milestone.create_from_soup(BeautifulSoupFactory.create_from_string(str(milestone))),
+                milestones_table.find_all("tr")))
 
     # Scrapes and returns games.
     def __get_games(self) -> List[Game]:
@@ -116,8 +102,5 @@ class PsnProfileScraper:
         if not self.__soup_level_history.select("table.box.zebra"):
             return []
 
-        levels = []
-        for level in self.__soup_level_history.select("table.box.zebra")[0].find_all("tr"):
-            levels.append(Level.create_from_soup(BeautifulSoupFactory.create_from_string(str(level))))
-
-        return levels
+        return list(map(lambda level: Level.create_from_soup(BeautifulSoupFactory.create_from_string(str(level))),
+                        self.__soup_level_history.select("table.box.zebra")[0].find_all("tr")))
